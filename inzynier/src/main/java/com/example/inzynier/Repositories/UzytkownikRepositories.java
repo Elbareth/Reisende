fde84ba22fc9dc9b6032ea86dd884fec69fccc63
Izabela <izab7212119@gmail.com>
2019-08-12 13:56:04 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/Repositories/UzytkownikRepositories.java
+++ inzynier/src/main/java/com/example/inzynier/Repositories/UzytkownikRepositories.java
@@ -3,8 +3,10 @@ package com.example.inzynier.Repositories;
 import com.example.inzynier.tables.Uzytkownik;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository("UzytkownikRepositories")
 public interface UzytkownikRepositories extends JpaRepository<Uzytkownik, String> {
     Boolean existsByLoginAndHaslo(String login, String haslo);
+    ArrayList<Uzytkownik> findAllByOrderByDoswiadczenieDesc();
 }
--- inzynier/src/main/java/com/example/inzynier/Repositories/WiadomosciRepositories.java
+++ inzynier/src/main/java/com/example/inzynier/Repositories/WiadomosciRepositories.java
@@ -1,9 +1,13 @@
 package com.example.inzynier.Repositories;
 
+import com.example.inzynier.tables.Uzytkownik;
 import com.example.inzynier.tables.Wiadomosci;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository("WiadomosciRepositories")
 public interface WiadomosciRepositories extends JpaRepository<Wiadomosci, Integer> {
+    public List<Wiadomosci> findAllByOdbiorcaOrderByDataAsc(Uzytkownik odbiorca);
+    public List<Wiadomosci> findAllByNadawcaOrderByDataAsc(Uzytkownik nadawca);
 }
