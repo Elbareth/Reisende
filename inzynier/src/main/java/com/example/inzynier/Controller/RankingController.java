5248655d0e6b886967b7542f84a4895facb84cff
Izabela <izab7212119@gmail.com>
2019-08-12 14:28:07 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/Controller/RankingController.java
+++ inzynier/src/main/java/com/example/inzynier/Controller/RankingController.java
@@ -5,6 +5,7 @@ import com.example.inzynier.Service.UzytkownikService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +19,7 @@ public class RankingController {
     private UzytkownikService uzytkownikService;
 
     @GetMapping("/ranking")
-    public ModelAndView getRanking(@RequestParam(required = false) Integer page, HttpSession sesja){
+    public ModelAndView getRanking(@RequestParam(required = false) Integer page, HttpSession sesja, ModelMap modelMap){
         if(sesja.getAttribute("login")==null){
             return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
         }
@@ -29,8 +30,18 @@ public class RankingController {
         modelAndView.addObject("maxPages", pagedListHolder.getPageCount()); // znajdujemy maksymalna liczbe naszych uzytkownikow
         if(page==null || page < 1 || page > pagedListHolder.getPageCount())page=1; // w przypadku gdy nie rozpoznajemy strony wracamy n strone pierwsza
         modelAndView.addObject("page", page); // i dodajemy ja do naszego modelAndView
-        if(page == null || page < 1 || page > pagedListHolder.getPageCount()){
-            pagedListHolder.setPage(0);
+        if(page == null || page < 1 || page > pagedListHolder.getPageCount()){ // w tym miejscu moge ustawic strone dla uzytkownika
+            //pagedListHolder.setPage(0);
+            String login = sesja.getAttribute("login").toString();
+            modelMap.put("login",login);
+            List<UzytkownikDTO> listTmp = pagedListHolder.getPageList();
+            for(int i=0;i<listTmp.size();i++){
+                if(listTmp.get(i).getLogin().equals(login)){
+                    pagedListHolder.setPage(0);
+                    break;
+                }
+                else pagedListHolder.setPage(0);
+            }
             modelAndView.addObject("uzytkownikLista", pagedListHolder.getPageList());
         }
         else if(page <= pagedListHolder.getPageCount()) {
