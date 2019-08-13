726b332e90c2ae87b80a85230fb32722c0998b5c
Izabela <izab7212119@gmail.com>
2019-08-13 15:20:42 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/Controller/RankingController.java
+++ inzynier/src/main/java/com/example/inzynier/Controller/RankingController.java
@@ -1,47 +1,64 @@
 package com.example.inzynier.Controller;
 
 import com.example.inzynier.DTO.UzytkownikDTO;
+import com.example.inzynier.Repositories.UzytkownikRepositories;
 import com.example.inzynier.Service.UzytkownikService;
+import org.apache.tomcat.util.http.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import sun.plugin.AppletViewer;
+
+import java.awt.datatransfer.DataFlavor;
 import java.util.*;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class RankingController {
     @Autowired
     private UzytkownikService uzytkownikService;
+    @Autowired
+    private UzytkownikRepositories uzytkownikRepositories;
 
     @GetMapping("/ranking")
     public ModelAndView getRanking(@RequestParam(required = false) Integer page, HttpSession sesja, ModelMap modelMap){
         if(sesja.getAttribute("login")==null){
             return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
         }
+        String search = "";
+        modelMap.put("search",search);
         List<UzytkownikDTO> uzytkownikList = uzytkownikService.listRaking();
         ModelAndView modelAndView = new ModelAndView("ranking.index","uzytkownikLista",uzytkownikList);
         PagedListHolder<UzytkownikDTO> pagedListHolder = new PagedListHolder<>(uzytkownikList); // tworzymy pagelistholder z nasza lista uzytkownikow
         pagedListHolder.setPageSize(20);
         modelAndView.addObject("maxPages", pagedListHolder.getPageCount()); // znajdujemy maksymalna liczbe naszych uzytkownikow
-        if(page==null || page < 1 || page > pagedListHolder.getPageCount())page=1; // w przypadku gdy nie rozpoznajemy strony wracamy n strone pierwsza
-        modelAndView.addObject("page", page); // i dodajemy ja do naszego modelAndView
-        if(page == null || page < 1 || page > pagedListHolder.getPageCount()){ // w tym miejscu moge ustawic strone dla uzytkownika
-            //pagedListHolder.setPage(0);
+        if(page==null || page < 1 || page > pagedListHolder.getPageCount()){
+            //page=1; // w przypadku gdy nie rozpoznajemy strony wracamy n strone pierwsza
             String login = sesja.getAttribute("login").toString();
             modelMap.put("login",login);
-            List<UzytkownikDTO> listTmp = pagedListHolder.getPageList();
+            List<UzytkownikDTO> listTmp = uzytkownikService.listRaking();
             for(int i=0;i<listTmp.size();i++){
                 if(listTmp.get(i).getLogin().equals(login)){
-                    pagedListHolder.setPage(0);
+                    pagedListHolder.setPage((int)(i/19)+1);
+                    page = (i/19)+1;
                     break;
                 }
-                else pagedListHolder.setPage(0);
+                else{
+                    pagedListHolder.setPage(1);
+                    page = 1;
+                }
             }
+        }
+        modelAndView.addObject("page", page); // i dodajemy ja do naszego modelAndView
+        if(page == null || page < 1 || page > pagedListHolder.getPageCount()){ // w tym miejscu moge ustawic strone dla uzytkownika
+            pagedListHolder.setPage(0);
             modelAndView.addObject("uzytkownikLista", pagedListHolder.getPageList());
         }
         else if(page <= pagedListHolder.getPageCount()) {
@@ -50,5 +67,18 @@ public class RankingController {
         }
         return modelAndView;
 
+    }
+    @PostMapping("/search")
+    public ModelAndView search(@RequestParam("search") String search, HttpSession sesja, ModelMap modelMap){
+        if(sesja.getAttribute("login")==null){
+            return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
+        }
+        if(!uzytkownikRepositories.findByLogin(search).isPresent()){
+            //TODO Alert
+            throw new EntityNotFoundException("Nie ma takiego uzytkownika");
+        }
+        List<UzytkownikDTO> listTmp = new ArrayList<>();
+        listTmp.add(uzytkownikService.findByLogin(search));
+        return new ModelAndView ("ranking.index", "uzytkownikLista", listTmp);
     }
 }
--- inzynier/src/main/java/com/example/inzynier/Controller/WiadomoscController.java
+++ inzynier/src/main/java/com/example/inzynier/Controller/WiadomoscController.java
@@ -2,18 +2,18 @@ package com.example.inzynier.Controller;
 
 import com.example.inzynier.DTO.UzytkownikDTO;
 import com.example.inzynier.DTO.WiadomosciDTO;
+import com.example.inzynier.Repositories.UzytkownikRepositories;
 import com.example.inzynier.Service.UzytkownikService;
 import com.example.inzynier.Service.WiadomosciService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -22,6 +22,8 @@ public class WiadomoscController {
     private WiadomosciService wiadomosciService;
     @Autowired
     private UzytkownikService uzytkownikService;
+    @Autowired
+    private UzytkownikRepositories uzytkownikRepositories;
 
     @GetMapping("/wiadomosc")
     public ModelAndView getListaOdbiorca(@RequestParam(required = false) Integer page, HttpSession sesja){
@@ -55,15 +57,63 @@ public class WiadomoscController {
         return new ModelAndView("otworzWiadomosc.index", "wiadomosc",wiadomosciDTO);
     }
 
-    @DeleteMapping("/delete/{typ}/{id}")
+    @PostMapping("/delete/{typ}/{id}")
     public ModelAndView deleteWindow(@PathVariable("id") Integer id,@PathVariable("typ") String typ, HttpSession sesja){
         wiadomosciService.deleteWiadomosc(id);
         if(typ.equals("nadawca")) return getListNadawca(1,sesja);
         return getListaOdbiorca(1,sesja);
     }
 
-    //odpisywanie na wiadomosci na podstawie starego kodu
-    //pisanie wiadomosci skopuj i zdebuguj
+    @GetMapping("/napiszWiadomosc")
+    public ModelAndView napiszWiadomosc(HttpSession sesja){
+        if(sesja.getAttribute("login")==null){
+            return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
+        }
+        return new ModelAndView("napiszWiadomosc.index","wiadomosc",new WiadomosciDTO());
+    }
+    @PostMapping("/wyslijWiadomosc")
+    public ModelAndView wyslijWiadomosc(@ModelAttribute("wiadomosc")WiadomosciDTO wiadomosc, HttpSession sesja){
+        if(sesja.getAttribute("login")==null){
+            return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
+        }
+        if(!uzytkownikRepositories.findByLogin(wiadomosc.getOdbiorca()).isPresent()){
+            //TODO Alert
+            return new ModelAndView("napiszWiadomosc.index","wiadomosc",new WiadomosciDTO());
+        }
+        String login = sesja.getAttribute("login").toString();
+        wiadomosc.setNadawca(login);
+        wiadomosc.setData(LocalDate.now());
+        wiadomosciService.createWiadomosc(wiadomosc);
+        return getListaOdbiorca(1,sesja);
+    }
+
+    @GetMapping("/odpiszWiadomosc/{odbiorca}")
+    public ModelAndView odpiszWiadomosc(@PathVariable("odbiorca") String odbiorca, HttpSession sesja, ModelMap modelMap){
+        if(sesja.getAttribute("login")==null){
+            return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
+        }
+        WiadomosciDTO wiadomosciDTO = new WiadomosciDTO();
+        wiadomosciDTO.setOdbiorca(odbiorca);
+        modelMap.put("wiadomoscTmp",wiadomosciDTO);
+        return new ModelAndView("odpiszNaWiadomosc.index","wiadomosc",wiadomosciDTO);
+    }
+
+    @PostMapping(value = "/wyslijRE/{odbiorca}", consumes = "multipart/form-data", produces = { "application/json", "application/xml" })
+    public ModelAndView wyslijRE(@PathVariable("odbiorca") String odbiorca, @ModelAttribute("wiadomosc") WiadomosciDTO wiadomosc, HttpSession sesja){
+        if(sesja.getAttribute("login")==null){
+            return new ModelAndView("login","uzytkownik",new UzytkownikDTO());
+        }
+        if(!uzytkownikRepositories.findByLogin(odbiorca).isPresent()){
+            //TODO Alert
+            return new ModelAndView("napiszWiadomosc.index","wiadomosc",new WiadomosciDTO());
+        }
+        wiadomosc.setOdbiorca(odbiorca);
+        String login = sesja.getAttribute("login").toString();
+        wiadomosc.setNadawca(login);
+        wiadomosc.setData(LocalDate.now());
+        wiadomosciService.createWiadomosc(wiadomosc);
+        return getListaOdbiorca(1,sesja);
+    }
 
     private ModelAndView createPagination(List<WiadomosciDTO> wiadomosciList, ModelAndView modelAndView, Integer page){
         PagedListHolder<WiadomosciDTO> pagedListHolder = new PagedListHolder<>(wiadomosciList); // tworzymy pagelistholder z nasza lista uzytkownikow
