ac3fbe1e9658c82e581b1963b3ffbadfddddb6ae
Izabela <izab7212119@gmail.com>
2019-08-13 15:17:07 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/Assembler/WiadomosciAssembler.java
+++ inzynier/src/main/java/com/example/inzynier/Assembler/WiadomosciAssembler.java
@@ -1,6 +1,7 @@
 package com.example.inzynier.Assembler;
 
 import com.example.inzynier.DTO.WiadomosciDTO;
+import com.example.inzynier.Service.UzytkownikService;
 import com.example.inzynier.tables.Wiadomosci;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,10 +12,13 @@ import java.util.List;
 @Component
 public class WiadomosciAssembler {
     @Autowired
+    private UzytkownikService uzytkownikService;
+    @Autowired
     private UzytkownikAssembler uzytkownikAssembler;
     public WiadomosciDTO toDto(Wiadomosci wiadomosci){
-        return new WiadomosciDTO(uzytkownikAssembler.toDto(wiadomosci.getNadawca()), uzytkownikAssembler.toDto(wiadomosci.getOdbiorca()), wiadomosci.getTytul(), wiadomosci.getTresc(), wiadomosci.getData());
+        return new WiadomosciDTO(wiadomosci.getNadawca().getLogin(), wiadomosci.getOdbiorca().getLogin(), wiadomosci.getTytul(), wiadomosci.getTresc(), wiadomosci.getData());
     }
+
     public List<WiadomosciDTO> toDto(List<Wiadomosci> listaWiadomosci){
         List<WiadomosciDTO> listaWiadomosciDto = new ArrayList<>();
         listaWiadomosci.forEach(param ->{
@@ -22,9 +26,23 @@ public class WiadomosciAssembler {
         });
         return listaWiadomosciDto;
     }
-    public Wiadomosci toEntity(WiadomosciDTO wiadomosciDTO){
-        return new Wiadomosci(uzytkownikAssembler.toEntity(wiadomosciDTO.getNadawca()), uzytkownikAssembler.toEntity(wiadomosciDTO.getOdbiorca()), wiadomosciDTO.getTytul(), wiadomosciDTO.getTresc(), wiadomosciDTO.getData());
+
+    public WiadomosciDTO toDtoWithId(Wiadomosci wiadomosci){
+        return new WiadomosciDTO(wiadomosci.getId(),wiadomosci.getNadawca().getLogin(), wiadomosci.getOdbiorca().getLogin(), wiadomosci.getTytul(), wiadomosci.getTresc(), wiadomosci.getData());
     }
+
+    public List<WiadomosciDTO> toDtoWithId(List<Wiadomosci> listaWiadomosci){
+        List<WiadomosciDTO> listaWiadomosciDto = new ArrayList<>();
+        listaWiadomosci.forEach(param ->{
+            listaWiadomosciDto.add(toDtoWithId(param));
+        });
+        return listaWiadomosciDto;
+    }
+
+    public Wiadomosci toEntity(WiadomosciDTO wiadomosciDTO){
+        return new Wiadomosci(uzytkownikAssembler.toEntity(uzytkownikService.findByLogin(wiadomosciDTO.getNadawca())), uzytkownikAssembler.toEntity(uzytkownikService.findByLogin(wiadomosciDTO.getOdbiorca())), wiadomosciDTO.getTytul(), wiadomosciDTO.getTresc(), wiadomosciDTO.getData());
+    }
+
     public List<Wiadomosci> toEntity(List<WiadomosciDTO> listaWiadomosciDto){
         List<Wiadomosci> listaWiadomosci = new ArrayList<>();
         listaWiadomosciDto.forEach(param ->{
