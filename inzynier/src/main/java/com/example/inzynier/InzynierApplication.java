3f9dcee0cd73d977804b289d1283be38165e4bdc
Izabela <izab7212119@gmail.com>
2019-08-14 16:08:19 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/InzynierApplication.java
+++ inzynier/src/main/java/com/example/inzynier/InzynierApplication.java
@@ -1,13 +1,19 @@
 package com.example.inzynier;
 
+import com.example.inzynier.Thread.PaskiZycia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class InzynierApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InzynierApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(InzynierApplication.class, args);
+		PaskiZycia zycie = new PaskiZycia();
+		applicationContext.getAutowireCapableBeanFactory().autowireBean(zycie);
+		Thread thread = new Thread(zycie);
+		thread.start();
 	}
 
 }
