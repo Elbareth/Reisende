63f3ecfa9fc30e687534186377adbfffea7280fb
Izabela <izab7212119@gmail.com>
2019-08-20 15:34:48 +0200

    Add files via upload

--- inzynier/src/main/java/com/example/inzynier/Thread/PaskiZycia.java
+++ inzynier/src/main/java/com/example/inzynier/Thread/PaskiZycia.java
@@ -1,27 +1,39 @@
 package com.example.inzynier.Thread;
 
+import com.example.inzynier.BasicService.PoborMonet;
 import com.example.inzynier.DTO.UzytkownikDTO;
 import com.example.inzynier.Service.UzytkownikService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
 
 public class PaskiZycia implements Runnable {
     @Autowired
     private UzytkownikService uzytkownikService;
+    private PoborMonet pobor = new PoborMonet();
     @Override
     public void run() {
-        //TODO po sprawdzeniu chce zeby paski zycia aktualizowaly sie -2 pukty na godzine
         long lastTime = System.currentTimeMillis();
         while(true){
             long test = System.currentTimeMillis();
-            if(test - lastTime >=  3600000){
+            if(test - lastTime >=  3600000){ // TODO przekierowanie do odp strony
                 List<UzytkownikDTO> all = uzytkownikService.findAll();
                 for(int i=0;i<all.size();i++){
                     all.get(i).setHigiena(all.get(i).getHigiena()-2);
                     all.get(i).setSen(all.get(i).getSen()-2);
                     all.get(i).setGlod(all.get(i).getGlod()-2);
                     all.get(i).setPragnienie(all.get(i).getPragnienie()-2);
+                    if(all.get(i).getHigiena() <= 0 ||
+                       all.get(i).getSen() <= 0     ||
+                       all.get(i).getGlod() <= 0    ||
+                       all.get(i).getPragnienie() <= 0){
+                            all.get(i).setHigiena(0);
+                            all.get(i).setSen(0);
+                            all.get(i).setGlod(0);
+                            all.get(i).setPragnienie(0);
+                            pobor.poborMonet(10, all.get(i));
+                    }
                     uzytkownikService.update(all.get(i).getLogin(), all.get(i));
                 }
                 lastTime = System.currentTimeMillis();
