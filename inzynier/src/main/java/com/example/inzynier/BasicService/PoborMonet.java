6271f8da7268111e8e37fd3d7ae7ef469429a481
Izabela <izab7212119@gmail.com>
2019-08-20 15:30:40 +0200

    Add files via upload

---
+++ inzynier/src/main/java/com/example/inzynier/BasicService/PoborMonet.java
@@ -0,0 +1,14 @@
+package com.example.inzynier.BasicService;
+
+import com.example.inzynier.DTO.UzytkownikDTO;
+
+public class PoborMonet {
+
+    public UzytkownikDTO poborMonet(Integer ile, UzytkownikDTO uzytkownikDTO){
+        uzytkownikDTO.setZloteMonety(uzytkownikDTO.getZloteMonety()-ile);
+        if(uzytkownikDTO.getZloteMonety() <= 0){
+            uzytkownikDTO.setZloteMonety(0);
+        }
+        return uzytkownikDTO;
+    }
+}
